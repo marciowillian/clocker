40acc9ae063c02cec8f8acd60230c5c8b0896d79
Marcio Willian <willian_mw.cc@hotmail.com>
2021-09-05 21:01:35 -0300

    D - Trabalhando no layout do component de agenda

--- pages/agenda.js
+++ pages/agenda.js
@@ -1,20 +1,54 @@
-import { useEffect } from "react"
-import { useRouter } from "next/router"
-import { Button } from "@chakra-ui/react"
-import { useAuth } from "./../components"
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { useFetch } from '@refetty/react'
+
+import { Button, Container, Box, IconButton } from '@chakra-ui/react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
+
+import { useAuth, Logo, formatDate } from './../components'
+
+const getAgenda = ({ token, when }) =>
+  axios({
+    method: 'get',
+    url: '/api/agenda',
+    params: { when },
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+const Header = ({ children }) => (
+  <Box p={4} display="flex" alignItems="center" justifyContent="space-between">
+    {children}
+  </Box>
+)
 
 export default function Agenda() {
   const [auth, { logout }] = useAuth()
   const router = useRouter()
+  const [when, setWhen] = useState(() => new Date())
+  // const [data, { loading, status, error }, fetch] = useFetch(() =>
+  //   getAgenda(when)
+  // )
 
   useEffect(() => {
     !auth.user && router.push('/')
   }, [auth.user])
 
   return (
-      <div>
+    <Container>
+      <Header>
+        <Logo size={150} />
         <Button onClick={logout}>Sair</Button>
-      </div>
-    )
-  }
-  
+      </Header>
+      <Box mt={8} display="flex" alignItems="center">
+        <IconButton icon={<ChevronLeftIcon />} bg="transparent" />
+        <Box flex={1} textAlign="center">
+          {formatDate(new Date(), 'PPPP')}
+        </Box>
+        <IconButton icon={<ChevronRightIcon />} bg="transparent" />
+      </Box>
+    </Container>
+  )
+}
